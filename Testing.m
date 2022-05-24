4a837edc0c017e45facaeee34b730b8da32af59d
ardanvf <71808926+ardanvf@users.noreply.github.com>
2022-05-24 11:33:10 +0700

    add backpropagation

---
+++ Testing.m
@@ -0,0 +1,13 @@
+load('data_uji.mat');
+jumlahData = 3;
+y = sim(net, data_uji);
+kelas = zeros(1, jumlahData);
+disp('Hasil Pengujian');
+
+for(a = 1 : jumlahData)
+    if(y(a) >= 0.5)
+        disp('Kelas 2');
+    else
+        disp('Kelas 1');
+    end
+end
